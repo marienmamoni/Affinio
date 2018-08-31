dd91b228bc5beddf8cb80c28a0c2216ffc63fb72
Marie-Louise Nsele <malou.nsele@gmail.com>
2018-08-30 22:43:59 -0400

    Code challenge: fix when list of two persons

--- src/test/scala/com/affin/CodeChallengeTests.scala
+++ src/test/scala/com/affin/CodeChallengeTests.scala
@@ -13,6 +13,27 @@ class CodeChallengeTests  extends FunSuite {
   }
 
 
+  test("Gift challenge with a list of two persons") {
+    val listPeople = List("Ardi", "Quan")
+    val codeChallenge = new CodeChallenge()
+    val givenList = codeChallenge.exchangeGift(listPeople)
+    val givenList2 = codeChallenge.exchangeGift(listPeople)
+    println(s"Given list 1 = $givenList")
+    println(s"Given list 2 = $givenList2")
+
+    assert(givenList.nonEmpty == true)
+    assert(givenList.size == 2) // every person gives one gift
+    assert(givenList.distinct.size == givenList.size) // all the combinations for givenList1 are unique
+
+    assert(givenList2.nonEmpty == true)
+    assert(givenList2.size == 2) // every person gives one gift
+    assert(givenList2.distinct.size == givenList.size) // all the combinations for givenList2 are unique
+
+    assert(givenList != givenList2) // the random shuffle generates two different lists
+
+  }
+
+
   test("Gift challenge with a list of five persons") {
     val listPeople = List("Phil", "Stephen", "Ardi", "Quan", "Joe")
     val codeChallenge = new CodeChallenge()
